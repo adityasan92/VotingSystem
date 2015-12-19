01b5c22da448201f2479dba1d5513ccef89fdcab
adityasan92 <adityasan92@gmail.com>
2015-12-18 19:02:12 -0500

    Added webpack support

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ export const store = makeStore();
 startServer();
 
 startServer(store);
+
+store.dispatch({
+  type: 'SET_ENTRIES',
+  entries: require('./entries.json')
+});
+store.dispatch({type: 'NEXT'});
