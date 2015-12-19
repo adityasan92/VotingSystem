01d344a2b84fecb77b5873928b86ba1d42ab1f53
adityasan92 <adityasan92@gmail.com>
2015-12-18 21:11:02 -0500

    Some more changes

--- webpack.config.js
+++ webpack.config.js
@@ -11,9 +11,6 @@ module.exports = {
       test: /\.jsx?$/,
       exclude: /node_modules/,
       loader: 'react-hot!babel'
-    }, {
-      test: /\.css$/,
-      loader: 'style!css!autoprefixer?browsers=last 2 versions'
     }]
   },
   resolve: {
